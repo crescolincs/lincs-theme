f8a659ca0e85c8f09b72b2d298ee3707cefbcd3a
dklongley <dklongley@gmail.com>
2024-03-28 10:39:06 +0000

    add js to static/js

---
+++ static/js/sectordropdown.js
@@ -0,0 +1,22 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dodd() { //dodd: do dropdown
+    document.getElementById("sector-dropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropdown')) {
+      var dropdowns = document.getElementsByClassName("dropdown-menu");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  //
+  function openmobmenu(){document.getElementById("navbarSupportedContent").classList.toggle("show");}
