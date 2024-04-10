0e0d45d03008ea1bce0182e1532b5e3ae0d18aa0
dklongley <dklongley@gmail.com>
2024-04-10 18:33:46 +0100

    debug hzd

--- static/js/es-hzd.js
+++ static/js/es-hzd.js
@@ -1,1 +1,154 @@
-var HoriZontaDelic=function(){return{clicked:0,scrollbehaviour:"smooth",gap:0,clickuptarget:0,clickdowntarget:0,init:function(t){var i=this;this.addEvent("resize",window,function(t){i.setSizes()}),this.addEvent("keydown",window,function(t){i.setKeys(t)}),t&&t.id&&(this.hzdscroller=document.getElementById(t.id)),this.hzdscroller||(this.isIE||this.isFirefox?this.hzdscroller=document.documentElement:this.hzdscroller=document.body),t&&t.gap?this.gap=t.gap:this.gap=0,this.setSizes(),t&&t.prev&&(this.prev=document.getElementById(t.prev),this.addEvent("click",this.prev,function(t){i.clickDown(t)})),t&&t.next&&(this.next=document.getElementById(t.next),this.addEvent("click",this.next,function(t){i.clickUp(t)}))},addEvent:function(t,i,e){i.addEventListener?i.addEventListener(t,e,!1):i.attachEvent?i.attachEvent("on"+t,e):i[t]=e},setSizes:function(t){this.imagewidth=this.hzdscroller.children[0].clientWidth+this.gap,this.fullimagesinview=Math.trunc(this.hzdscroller.clientWidth/this.imagewidth),this.totalimages=this.hzdscroller.children.length,this.jumpsteps=Array(this.totalimages/this.fullimagesinview).fill(0).map((t,i)=>i*this.fullimagesinview+this.fullimagesinview)},setKeys:function(t){switch(t.key){case"ArrowRight":this.clickUp();break;case"ArrowDown":this.clicked=this.maxjumps,this.clickUp();break;case"ArrowLeft":this.clickDown();break;case"ArrowUp":this.clicked=-1*this.jumpsize,this.clickDown()}},cancelEvent:function(t){t||(t=window.event),t.target&&"IMG"===t.target.nodeName?t.preventDefault():t.srcElement&&"IMG"===t.srcElement.nodeName&&(t.returnValue=!1)},clickDown:function(t){this.clicked--,this.clickdowntarget=this.jumpsteps[this.clicked]-1,void 0===this.jumpsteps[this.clicked]?(this.hzdscroller.children[0].scrollIntoView({behavior:"smooth",block:"start",inline:"nearest"}),this.clicked=0):this.hzdscroller.children[this.jumpsteps[this.clicked]-1].scrollIntoView({behavior:"smooth",block:"start",inline:"nearest"})},clickUp:function(t){this.clicked++,this.clickuptarget=this.jumpsteps[this.clicked]-1,void 0===this.jumpsteps[this.clicked]?this.hzdscroller.children[this.hzdscroller.children.length-1].scrollIntoView({behavior:"smooth",block:"start",inline:"nearest"}):this.hzdscroller.children[this.jumpsteps[this.clicked]-1].scrollIntoView({behavior:"smooth",block:"start",inline:"nearest"})}}};
+/*
+ * horizontadelic - using dom overflow:scroll
+ * by dindinet
+ */
+var HoriZontaDelic = function () {
+    'use strict';
+    
+    var module = {
+        //axis: 'x',
+        clicked: 0,
+        scrollbehaviour: 'smooth',
+        gap: 0,
+        clickuptarget: 0,
+        clickdowntarget: 0,
+        /**
+         * @method init
+         */
+        init: function (options) {
+          //console.log(options);
+            var me = this;
+            this.addEvent('resize',window,function(e){me.setSizes()})//reset the size of the scroll jump when browser changes size
+            if (options && options.arrowkeys) {
+            this.addEvent('keydown',window,function(e){me.setKeys(e)})
+            }
+            //this.options = options;
+            
+            // find target element scroller or fall back to body
+            if (options && options.id) {
+                this.hzdscroller = document.getElementById(options.id);
+            }
+            if (!this.hzdscroller) {
+                if (this.isIE || this.isFirefox) {
+                    this.hzdscroller = document.documentElement;
+                } else {
+                    this.hzdscroller = document.body;
+                }
+            }
+            if (options && options.gap){this.gap = options.gap}else{this.gap = 0}
+            
+            this.setSizes()
+            
+            // if scroll options exist add events
+            if (options && options.prev) {
+                this.prev = document.getElementById(options.prev);
+                this.addEvent('click', this.prev, function (e) {
+                    me.clickDown(e);
+                });
+            }
+            if (options && options.next) {
+                this.next = document.getElementById(options.next);
+                this.addEvent('click', this.next, function (e) {
+                    me.clickUp(e);
+                });
+  
+            }
+  
+        },
+        /**
+         * @method addEvent
+         */
+        addEvent: function (name, el, func) {
+            if (el.addEventListener) {
+                el.addEventListener(name, func, false);
+            } else if (el.attachEvent) {
+                el.attachEvent('on' + name, func);
+            } else {
+                el[name] = func;
+            }
+        },
+              /**
+        * @method getjumpsteps
+        */
+        getjumpsteps: function (totimgs,imagesinview) {
+             var inttotsteps = Math.trunc(totimgs/imagesinview);
+             var inttotstepsimagesinview = inttotsteps*imagesinview;
+             var jumpsteps = Array(inttotstepsimagesinview/imagesinview).fill(0).map((e,i)=>(i*imagesinview)+imagesinview)
+             if(inttotstepsimagesinview < totimgs){jumpsteps.push(totimgs)}
+             return jumpsteps
+             },
+        /**
+        * @method setSizes
+        */
+        setSizes: function (e) {
+          this.imagewidth = this.hzdscroller.children[0].clientWidth + this.gap;
+          this.fullimagesinview = Math.trunc(this.hzdscroller.clientWidth/this.imagewidth)
+          this.totalimages = this.hzdscroller.children.length
+          this.jumpsteps = this.getjumpsteps(this.totalimages,this.fullimagesinview)
+       },
+        /**
+        * @method setKeys
+        */
+        setKeys: function (e) {
+          //console.log('setting Keys')
+          switch (e.key) {
+            case "ArrowRight":
+               this.clickUp();
+            break;
+            case "ArrowDown":
+              this.clicked = this.maxjumps;
+              this.clickUp();
+            break;
+            case "ArrowLeft":
+              this.clickDown();
+            break;
+            case "ArrowUp":
+              this.clicked = this.jumpsize*-1;
+              this.clickDown();
+            break;
+         }
+       },
+        /**
+         * @method cancelEvent
+         */
+        cancelEvent: function (e) {
+            if (!e) { e = window.event; }
+            if (e.target && e.target.nodeName === 'IMG') {
+                e.preventDefault();
+            } else if (e.srcElement && e.srcElement.nodeName === 'IMG') {
+                e.returnValue = false;
+            }
+        },
+        /**
+         * @method clickDown
+         */
+        clickDown: function (e) {
+            this.clicked--
+            this.clickdowntarget = this.jumpsteps[this.clicked]-1
+            if(this.jumpsteps[this.clicked] === undefined ){
+              //console.log('clickDown 👎')
+              this.hzdscroller.children[0].scrollIntoView({ behavior: "smooth"})
+              this.clicked = 0
+            }else{
+              //console.log('clickDown 👍')
+              this.hzdscroller.children[this.jumpsteps[this.clicked]-1].scrollIntoView({ behavior: "smooth"})
+            }
+        },
+        /**
+         * @method clickUp
+         */
+        clickUp: function (e) {
+          this.clicked++
+          this.clickuptarget = this.jumpsteps[this.clicked]-1
+          if(this.jumpsteps[this.clicked] === undefined ){  
+            //console.log('clickUp 👎')
+            this.hzdscroller.children[this.hzdscroller.children.length-1].scrollIntoView({ behavior: "smooth"})
+          }else{
+            //console.log('clickUp 👍')
+            this.hzdscroller.children[this.jumpsteps[this.clicked]-1].scrollIntoView({ behavior: "smooth"})
+          }
+  
+        }, 
+    };
+    return module;
+  };
