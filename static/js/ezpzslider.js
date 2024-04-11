ef81e541ceee0ad75077e71262036df8ba1e899b
dklongley <dklongley@gmail.com>
2024-04-11 13:15:27 +0100

    ezpzslider test 2

--- static/js/ezpzslider.js
+++ static/js/ezpzslider.js
@@ -1,12 +1,12 @@
 var button = document.getElementById("next");
 button.onclick = function () {
-  var container = document.getElementById("container");
+  var container = document.getElementById("ezpzwrap");
   sideScroll(container, "right", 50, container.clientWidth, 100);
 };
 
 var back = document.getElementById("prev");
 back.onclick = function () {
-  var container = document.getElementById("container");
+  var container = document.getElementById("ezpzwrap");
   sideScroll(container, "left", 50, container.clientWidth, 100);
 };
 
