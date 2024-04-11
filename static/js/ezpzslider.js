c0260701ef0cee9a735fff08f41bb301a557860d
dklongley <dklongley@gmail.com>
2024-04-11 12:54:08 +0100

    scrollbar white

---
+++ static/js/ezpzslider.js
@@ -0,0 +1,26 @@
+var button = document.getElementById("next");
+button.onclick = function () {
+  var container = document.getElementById("container");
+  sideScroll(container, "right", 50, container.clientWidth, 100);
+};
+
+var back = document.getElementById("prev");
+back.onclick = function () {
+  var container = document.getElementById("container");
+  sideScroll(container, "left", 50, container.clientWidth, 100);
+};
+
+function sideScroll(element, direction, speed, distance, step) {
+  scrollAmount = 0;
+  var slideTimer = setInterval(function () {
+    if (direction == "left") {
+      element.scrollLeft -= step;
+    } else {
+      element.scrollLeft += step;
+    }
+    scrollAmount += step;
+    if (scrollAmount >= distance) {
+      window.clearInterval(slideTimer);
+    }
+  }, speed);
+}
